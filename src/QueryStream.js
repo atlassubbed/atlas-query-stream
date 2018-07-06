3fc01ea8a56c99e678bfa7ead632c7d14d0a3d46
atlassubbed <atlassubbed@gmail.com>
2018-07-06 18:22:58 -0400

    implements instantiation and corresponding tests

--- src/QueryStream.js
+++ src/QueryStream.js
@@ -4,6 +4,10 @@ const { isArr } = require("./util");
 module.exports = class QueryStream extends Transform {
   constructor(...queries){
     super({objectMode: true})
+    if (!queries.length) 
+      throw new Error("requires at least one query");
+    const cur = {}, levs = [];
+    let jobs = queries, id = 0;
     this.query = node => {
       // todo
     }
--- test/QueryStream.test.js
+++ test/QueryStream.test.js
@@ -1,9 +1,14 @@
 const { describe, it } = require("mocha")
 const { expect } = require("chai")
+const { Transform } = require("stream");
+const QueryStream = require("../src/QueryStream");
 
-// default placeholder test
-describe("package", function(){
-  it("exists", function(){
-    expect(true).to.be.true;
+describe("QueryStream", function(){
+  it("should throw error if instantiated without query", function(){
+    expect(() => new QueryStream()).to.throw("requires at least one query")
+  })
+  it("should create an instance of a stream Transform", function(){
+    const queries = new QueryStream(() => {});
+    expect(queries).to.be.an.instanceOf(Transform)
   })
 })
